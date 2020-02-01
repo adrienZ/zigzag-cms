a3a56a3d317006f18984d648090f5d7adab099cf
AdrienZ <adrienzaganelli@gmail.com>
2020-02-01 17:17:05 +0100

    test

--- src/prod.js
+++ src/prod.js
@@ -2,7 +2,8 @@ module.exports = {
   'backend': {
     name: 'github',
     repo: 'adrienZ/zigzag-cms',
-    branch: 'master'
+    branch: 'master',
+    site_domain: 'cms.netlify.com'
   },
   publish_mode: 'editorial_workflow',
 }
